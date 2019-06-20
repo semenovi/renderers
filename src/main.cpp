72310fc5ebc073356ad2c3050da2b47437d5b612
semenovi <t79601712352@gmail.com>
2019-06-20 06:05:25 +0300

    can put the pixel. wow

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,75 @@
-#include "raytracing.h"
+/*
+When creating your project, uncheck OWL,
+uncheck Class Library, select Static
+instead of Dynamic and change the target
+model to Console from GUI.
+Also link glut.lib to your project once its done.
+*/
 
-raytracing rt;
-void r(int w, int h)
-{
-	glViewport(0, 0, w, h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0, w, 0, h);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+#include <windows.h>   // Standard Header For Most Programs
+#include <gl/gl.h>     // The GL Header File
+#include <gl/glut.h>   // The GL Utility Toolkit (Glut) Header
+#include <vector>
+
+const int W = 500;
+const int H = 500;
+
+void trace_ray(std::vector<GLint> O, D, t_min, t_max) {
+	closest_t = inf
+		closest_sphere = NULL
+		for sphere in scene.Spheres{
+			t1, t2 = IntersectRaySphere(O, D, sphere)
+			if t1 in[t_min, t_max] and t1 < closest_t
+				closest_t = t1
+				closest_sphere = sphere
+				if t2 in[t_min, t_max] and t2 < closest_t
+					closest_t = t2
+					closest_sphere = sphere
+		}
+			if closest_sphere == NULL
+				return BACKGROUND_COLOR
+				return closest_sphere.color
 }
-void d()
+
+void display(void)   // Create The Display Function
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	rt.draw();
+	glMatrixMode(GL_PROJECTION);  // Select The Projection Matrix
+	glLoadIdentity();									// Reset The Current Modelview Matrix
+	gluOrtho2D(0.0, (GLdouble)W, (GLdouble)H, 0.0);
+	glBegin(GL_POINTS);
+		glColor3f(1, 1, 1);
+		glVertex2i(100, 100);
+	glEnd();
 	glutSwapBuffers();
+	// Swap The Buffers To Not Be Left With A Clear Screen
 }
-void start(int &argc, char * argv[])
+
+void reshape(int w, int h)   // Create The Reshape Function (the viewport)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	glutCreateWindow("Raytracing");
-	glutReshapeFunc(r);
-	glutDisplayFunc(d);
-	glutIdleFunc(d);
-	glutMainLoop();
+	glutReshapeWindow(W, H);
 }
-int main(int argc, char * argv[])
+
+void keyboard(unsigned char key, int x, int y)  // Create Keyboard Function
 {
-	start(argc, argv);
-	return 0;
+	switch (key) {
+	case 27:        // When Escape Is Pressed...
+		exit(0);   // Exit The Program
+		break;        // Ready For Next Case
+	default:        // Now Wrap It Up
+		break;
+	}
 }
+
+void main(int argc, char** argv)   // Create Main Function For Bringing It All Together
+{
+	glutInit(&argc, argv); // Erm Just Write It =)
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE); // Display Mode
+	glutInitWindowSize(W, H); // If glutFullScreen wasn't called this is the window size
+	glutCreateWindow("Rasterizer"); // Window Title (argv[0] for current directory as title)
+	glutDisplayFunc(display);  // Matching Earlier Functions To Their Counterparts
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();          // Initialize The Main Loop
+}
+
